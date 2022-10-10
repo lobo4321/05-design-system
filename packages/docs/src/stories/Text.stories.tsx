9138e2d767362942b65de96fb2da8fcfe23ba741
João guilherme dos santos <jglobo47@gmail.com>
2022-10-10 20:21:14 -0300

    feat: add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem error sit ut iusto voluptatem expedita eaque, provident, dolor debitis corrupti cupiditate? Sapiente omnis iusto qui error dolor, incidunt aut nulla.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
