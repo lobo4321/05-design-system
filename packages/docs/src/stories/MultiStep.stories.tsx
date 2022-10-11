754b7db32663511c90e5374ad412457125b11c54
João guilherme dos santos <jglobo47@gmail.com>
2022-10-11 11:31:48 -0300

    ci: Add turbo repo cache on deploy build

--- packages/docs/src/stories/MultiStep.stories.tsx
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-design-ui/react'
 
 export default {
   title: 'Form/Multi Step',
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @ignite-design-ui/react
+
+## 2.0.0
+
+### Major Changes
+
+- Add display name to all components
